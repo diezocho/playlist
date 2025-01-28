993a2bd59265be04af08029703f24ba43c9abc8e
diezocho <joseolalde1234@yahoo.com>
2025-01-27 20:27:31 -0600

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,3 +1,3 @@
-import { put } from "@vercel/blob";
+import { put } from "playlist-three-rho.vercel.app"; 
 BLOB_READ_WRITE_TOKEN="vercel_blob_rw_BFgYzbSgT2wPy0Rx_Jk6sFwkF1TngnmdDKvPBoZLepvFLJE"
-const { url } = await put('articles/blob.txt', 'Hello World!', { access: 'public' });
+const { url } = https://bfgyzbsgt2wpy0rx.public.blob.vercel-storage.com/playlist/Combined_Playlist%20(1)-o3n1noyS2JDYYURncyvVYETKmUH5k8.m3u
