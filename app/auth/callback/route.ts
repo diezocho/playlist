ffdfb7c1f3d8960fac533513cc0a340b9fbed1d0
diezocho <joseolalde1234@yahoo.com>
2025-01-27 20:25:43 -0600

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,3 +1,3 @@
 import { put } from "@vercel/blob";
-
+BLOB_READ_WRITE_TOKEN="vercel_blob_rw_BFgYzbSgT2wPy0Rx_Jk6sFwkF1TngnmdDKvPBoZLepvFLJE"
 const { url } = await put('articles/blob.txt', 'Hello World!', { access: 'public' });
